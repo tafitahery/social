7e58575b1c24f00e4d8d4d8ba60ae952a9054e77
Tafita Hery <tafitahery@gmail.com>
2024-05-09 13:03:05 +0200

    Social Media Website Navbar UI Design

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,5 +1,43 @@
 import "./navbar.scss";
+import {
+  HomeOutlined,
+  DarkModeOutlined,
+  WbSunnyOutlined,
+  GridViewOutlined,
+  NotificationsOutlined,
+  EmailOutlined,
+  PersonOutlined,
+  SearchOutlined,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>Lebosocial</span>
+        </Link>
+        <HomeOutlined />
+        <DarkModeOutlined />
+        <GridViewOutlined />
+        <div className="search">
+          <SearchOutlined />
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlined />
+        <EmailOutlined />
+        <NotificationsOutlined />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <span>John Doe</span>
+        </div>
+      </div>
+    </div>
+  );
 }
