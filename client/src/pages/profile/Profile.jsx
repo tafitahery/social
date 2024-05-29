4eafad7a5a937fa0f152370691e9f9252e960e22
Tafita Hery <tafitahery@gmail.com>
2024-05-29 15:34:09 +0300

    Social Media Website User Profile Page UI Design

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,5 +1,72 @@
 import "./profile.scss";
+import {
+  EmailOutlined,
+  FacebookTwoTone,
+  Instagram,
+  Language,
+  LinkedIn,
+  MoreVert,
+  Pinterest,
+  Place,
+  Twitter,
+} from "@mui/icons-material";
+import Posts from "../../components/posts/Posts";
 
 export default function Profile() {
-  return <div className="profile">Profile</div>;
+  return (
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+          className="profilePic"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="https://facebook.com">
+              <FacebookTwoTone fontSize="large" />
+            </a>
+            <a href="https://facebook.com">
+              <Instagram fontSize="large" />
+            </a>
+            <a href="https://facebook.com">
+              <Twitter fontSize="large" />
+            </a>
+            <a href="https://facebook.com">
+              <LinkedIn fontSize="large" />
+            </a>
+            <a href="https://facebook.com">
+              <Pinterest fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Jane Doe</span>
+            <div className="info">
+              <div className="item">
+                <Place />
+                <span>USA</span>
+              </div>
+              <div className="item">
+                <Language />
+                <span>lebo.dev</span>
+              </div>
+            </div>
+            <button>Follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlined />
+            <MoreVert />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
 }
